1ab05475c0020e6105674b9245a43de198247577
Ashwiniborla <70287395+ashwiniborla@users.noreply.github.com>
2020-11-03 20:16:09 +0530

    Add files via upload

---
+++ ft2mt.py
@@ -0,0 +1,10 @@
+from tkinter import TK, Button ,Label,DoubleVar,Entry
+
+Window = TK()
+window.title("Feet to Meter Conversion App")
+window.configure(background="light green")
+window.geometry("320*220")
+window.resizable(width=False,height=False)
+
+
+window.mainloop()
